5eee88eabd98ee7433d2686fce078cb657ad0ef2
aabhasgulati <imaabhasgulati@gmail.com>
2021-08-31 16:22:34 +0530

    Update QuantumObjects.jl

--- src/QuantumObjects.jl
+++ src/QuantumObjects.jl
@@ -1,7 +1,7 @@
 module QuantumObjects
 import Base: *, ∘, kron, adjoint
 import LinearAlgebra: Hermitian, tr,ishermitian,norm
-export QuantumObject, Ket, Bra, DensityMatrix, inner,norm
+export QuantumObject, Ket, Bra, DensityMatrix, inner,norm,Ensemble
 
 abstract type QuantumObject end
 
@@ -41,6 +41,17 @@ struct DensityMatrix <: QuantumObject
         end
             new(Hermitian(rho))
     end
+    function DensityMatrix(ρ::Hermitian{ComplexF64, Matrix{ComplexF64}},to_normalize=true::Bool)
+        if !to_normalize & (real(tr(ρ)) > 1.0)
+            error("Density Matrix should have Trace smaller than or equal to  1")
+        elseif to_normalize 
+            ρ= ρ ./ tr(ρ)
+        end
+            new(ρ)
+    end 
+    function DensityMatrix(ket::Ket)
+        new(Hermitian(ket.coefficients'.*ket.coefficients))
+    end
 end
 
 inner(ρ::DensityMatrix, σ::DensityMatrix) = tr(ρ.matrix * σ.matrix)
@@ -54,5 +65,13 @@ Base.kron(b1::Bra, b2::Bra) = Bra(b1.k ⊗ b2.k)
 Base.kron(ρ1::DensityMatrix, ρ2::DensityMatrix) = DensityMatrix(ρ1.matrix ⊗ ρ2.matrix)
 
 ⊗ = Base.kron
+
+Matrix(ρ::DensityMatrix) =  ρ.matrix
+Ensemble(probs::Vector{Float64}, ρs::Vector{DensityMatrix}) = DensityMatrix(sum(probs.*Matrix.(ρs)),false)
+Ensemble(probs::Vector{Float64}, kets::Vector{Ket}) = Ensemble(probs,DensityMatrix.(kets))
+Ensemble(probs::Vector{Float64}, bras::Vector{Bra}) = Ensemble(probs,Ket.(Bra))
+
+
+
 
 end
